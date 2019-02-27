c2f50637d9f80df6ef9b25a28de6b146255ecaf2
dabaitudiu <bigbroase@163.com>
2019-02-28 01:04:21 +0800

    pet info

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # FindPetCaring
-Website[node.js, express js, postgres, bootstrap] allow pet owners to find places to deposit pets when they are not available. 
+
+### Table:Pet
+```SQL
+CREATE TABLE pet(
+   pname   VARCHAR(5) NOT NULL 
+  ,pid     VARCHAR(8) NOT NULL PRIMARY KEY
+  ,pmaster VARCHAR(8) NOT NULL
+  ,pclass  VARCHAR(6) NOT NULL
+  ,pold    INTEGER  NOT NULL
+  ,pgender VARCHAR(9) NOT NULL
+  ,purl    VARCHAR(23) 
+);
+```
+
---
+++ pet_info.sql
@@ -0,0 +1,38 @@
+CREATE TABLE mytable(
+   pname   VARCHAR(5) NOT NULL 
+  ,pid     VARCHAR(8) NOT NULL PRIMARY KEY
+  ,pmaster VARCHAR(8) NOT NULL
+  ,pclass  VARCHAR(6) NOT NULL
+  ,pold    INTEGER  NOT NULL
+  ,pgender VARCHAR(9) NOT NULL
+  ,purl    VARCHAR(23) 
+);
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Nohnd','696b809g','Zmupsohh','Cat',10,'ambiguous','http://ekuhmicoson.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Zneuh','840e590c','Zythourn','Dog',2,'ambiguous','http://homuoliduhol.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Stiel','875k534e','Hythorav','Hawk',8,'female','http://nuratjace.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Nlija','157d124h','Bmeskevi','Pigeon',4,'ambiguous','http://uratmeskthom.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Ridli','682l718d','Gythoros','Cat',8,'ambiguous','http://ohaeekuhekuh.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Buhel','381h268b','Iipacurs','Hawk',6,'male','http://uholomupljac.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Jhoco','145k929i','Usonipk','Owl',3,'ambiguous','http://mupsythoeleb.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Elamu','168c634l','Hnkua','Cat',10,'male','http://ljacpsonn.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Rlaev','285d327j','Dpsonlic','Dog',8,'ambiguous','http://ebrouholhomu.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Oevic','939l200d','Lalayhao','Owl',2,'female','http://uratelebkuli.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Kurvi','834g897f','Elidaipi','Dog',7,'ambiguous','http://thomelebipac.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Uomup','969i888g','Yevinjai','Pigeon',10,'female','http://mupspsonmesk.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Mljnd','904g121a','Cckullje','Pig',6,'female','http://ineleskelebr.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Yskid','338k280c','Kkeviica','Pigeon',2,'female','http://kalahomuronj.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Avine','882c668i','Dekuhela','Cat',6,'male','http://icohsonaelj.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Zaija','601g960h','Zkalaraa','Pigeon',5,'female','http://nkuhouhol.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Ymieb','236f600a','Bikalohl','Hawk',1,'female','http://ikalcekuidai.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Oliae','320f801h','Vekuhrom','Owl',6,'male','http://lidadaipronj.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Imeps','519d206i','Xskevulp','Pig',3,'male','http://omuponjahomu.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Yekel','567b674j','Oaythevl','Pig',6,'male','http://ohaenjamohae.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Lronj','873d296b','Vuholelo','Pigeon',7,'male','http://ackupsonckul.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Mkali','273g749l','Ficohytn','Dog',9,'ambiguous','http://ebroduramups.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Eicuh','342k826a','Fonoml','Pig',6,'male','http://amesinelnele.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Ikun','966b935b','Pnuhe','Pigeon',5,'ambiguous','http://natikuhol.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Ksoce','715c221h','Jcekupap','Dog',5,'male','http://lebrtikadura.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Nomon','903c961c','Tonjaonh','Cat',5,'female','http://idaiohaeevin.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Lkain','861g495g','Iuratraa','Cat',6,'female','http://ljacacekidai.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Sacku','113h761k','Cjameror','Pig',1,'female','http://ekuhlayttika.png');
+INSERT INTO mytable(pname,pid,pmaster,pclass,pold,pgender,purl) VALUES ('Oidoh','321h512k','Baeljuhp','Owl',3,'male','http://thomlebratik.png');
